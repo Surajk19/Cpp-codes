f066ebe59d6ebc85485587b0c6b744002fe1f94a
Suraj Khonde <144094073+Surajk19@users.noreply.github.com>
2025-08-28 02:20:25 +0530

    Delete DSA directory

--- DSA/1)Arrays/array3.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int arr[] {40, 50, 60, 70, 80, 90, 100}; // didn't need an assignment operator here
-    for (auto element: arr)
-        cout << element << " ";
-    return 0;
-}
